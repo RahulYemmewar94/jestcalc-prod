5b02eb4ea2859f6c9ecb1fa068248968e71b3a69
Yemmewarrahul <rahul.yemmewar>
2025-01-17 15:32:41 +0530

    failed test case for addition

--- src/calculator/calculator.test.js
+++ src/calculator/calculator.test.js
@@ -1,11 +1,21 @@
-
-import { render, screen, fireEvent } from "react-test-renderer";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CalculatorWithJestTestCases from "./calculator";
 
 describe("Calculator Test Cases", () => {
   test("Addition", async () => {
     render(<CalculatorWithJestTestCases />);
     
- 
+    fireEvent.click(screen.getByText("2"));
+    fireEvent.click(screen.getByText("1"));
+    fireEvent.click(screen.getByText("+"));
+    fireEvent.click(screen.getByText("6"));
+    fireEvent.click(screen.getByText("1"));
+    fireEvent.click(screen.getByText("5"));
+    fireEvent.click(screen.getByText("="));
+    
+    const result = await screen.findByTestId("result"); 
+    expect(result).toHaveTextContent("636"); 
   });
+
+ 
 });
